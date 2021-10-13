d7330c3f2945e051c7eac675374bfaf54e43c208
Davrosmester <zabodominik@gmail.com>
2021-10-13 17:58:08 +0200

    car and brand filled with basic attributes

--- IOUDIE_HFT_2021221.Models/Brand.cs
+++ IOUDIE_HFT_2021221.Models/Brand.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IOUDIE_HFT_2021221.Models
 {
+    [Table("brands")]
+
     class Brand
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+
+
     }
 }
--- IOUDIE_HFT_2021221.Models/Car.cs
+++ IOUDIE_HFT_2021221.Models/Car.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IOUDIE_HFT_2021221.Models
 {
+    [Table("cars")]
+   
     class Car
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        public int? BasePrice { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public int BrandId { get; set; }
     }
 }
