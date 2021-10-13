a23533d88f9a138bc79211f1725bb274cf160418
Davrosmester <zabodominik@gmail.com>
2021-10-13 18:10:20 +0200

    created Driver class and filled with basic attributes

---
+++ IOUDIE_HFT_2021221.Models/Drivers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOUDIE_HFT_2021221.Models
+{
+    [Table("drivers")]
+    class Drivers
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public bool DriversLicense { get; set; }
+
+    }
+}
