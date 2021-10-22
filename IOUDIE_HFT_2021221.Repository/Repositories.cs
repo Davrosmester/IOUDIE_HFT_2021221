1883b0a403bbd05b86fdeed414a30629f622231f
Davrosmester <zabodominik@gmail.com>
2021-10-22 10:30:19 +0200

    CRUD and normal repos in different .cs's

--- IOUDIE_HFT_2021221.Repository/Repositories.cs
+++ IOUDIE_HFT_2021221.Repository/Repositories.cs
@@ -8,35 +8,7 @@ using System.Threading.Tasks;
 
 namespace IOUDIE_HFT_2021221.Repository
 {
-    public abstract class Repositories<T> : IRepository<T> where T : class
-    {
-        protected DbContext ctx;
-
-        public Repositories(DbContext ctx)
-        {
-            this.ctx = ctx;
-        }
-
-        public void Create(T entity)
-        {
-            ctx.Set<T>().Add(entity);
-            ctx.SaveChanges();
-        }
-
-        public void Delete(T entity)
-        {
-            ctx.Set<T>().Remove(entity);
-            ctx.SaveChanges();
-        }
-
-        public IQueryable<T> GetAll()
-        {
-            return ctx.Set<T>();
-        }
-
-        public abstract T GetOne(int id);
-
-    }
+   
     public class CarRepository : Repositories<Car>, ICarShopRepository
     {
 
---
+++ IOUDIE_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOUDIE_HFT_2021221.Repository
+{
+    public abstract class Repositories<T> : IRepository<T> where T : class
+    {
+        protected DbContext ctx;
+
+        public Repositories(DbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            ctx.Set<T>().Remove(entity);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T GetOne(int id);
+
+    }
+}
