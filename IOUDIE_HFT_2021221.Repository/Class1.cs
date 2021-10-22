19959871cdd09c7f0f9e0f865ff14d993ff32178
Davrosmester <zabodominik@gmail.com>
2021-10-22 09:39:02 +0200

    IRepository done

--- IOUDIE_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IOUDIE_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ IOUDIE_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using IOUDIE_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOUDIE_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        T GetTOne(int id);
+        IQueryable<T> GetAll();
+        void Delete(T entity);
+        void Create(T entity); //only create not update
+    }
+    public interface ICarShopRepository : IRepository<Car>
+    {
+        void ChangePrice(int id, int newPrice); //update
+    }
+    public interface IBrandRepository : IRepository<Brand>
+    {
+        void ChangeBrandName(int id, string newBrandName);
+    }
+}
